5a77b2c67af4ed05094183af383438656f1495f1
nobre <henriquenobr@gmail.com>
2021-11-21 13:29:49 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
+
 
 function App() {
+
+  const [input, setInput] = useState("# Welcome to my React Markdown Previewer! \n ## This is a sub-heading... \n Heres some code, `<div></div>`, between 2 backticks. \n There's also [links](https://www.freecodecamp.org) \n ``` \n // this is multi-line code: \n function anotherExample(firstLine, lastLine) { \n   if (firstLine == '```' && lastLine == '```') { \n     return multiLineCode; \n   } \n } \n ``` \n 1. And there are numbered lists too. \n > Block Quotes! \n You can also make text **bold**... whoa! \n ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)")
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        autoFocus
+        id="editor"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <div id="preview">
+        <ReactMarkdown id="preview" children={input} remarkPlugins={[remarkGfm]} />
+      </div>
     </div>
   );
 }
